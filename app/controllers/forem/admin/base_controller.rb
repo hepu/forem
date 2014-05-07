276bbbe8c77546b63997f64d6954b9349015ad0e
Hans Gamarra <hans@appmeup.co>
2014-05-07 17:35:03 -0500

    [v1.1] Base controller actions passed to main app

--- app/controllers/forem/admin/base_controller.rb
+++ app/controllers/forem/admin/base_controller.rb
@@ -3,24 +3,8 @@ module Forem
     class BaseController < ForemApplicationController
       layout Forem.layout
       
-      before_filter :authenticate_forem_admin
-      before_filter :inside_admin_area
-
       def index
         # TODO: perhaps gather some stats here to show on the admin page?
-      end
-
-      private
-
-      def authenticate_forem_admin
-        if !forem_user || !forem_user.forem_admin?
-          flash.alert = t("forem.errors.access_denied")
-          redirect_to forums_path #TODO: not positive where to redirect here
-        end
-      end
-
-      def inside_admin_area
-        @inside_admin_area = true
       end
       
     end
--- lib/forem/version.rb
+++ lib/forem/version.rb
@@ -1,7 +1,7 @@
 module Forem
   class << self
     def version
-      %q{1.0.18}
+      %q{1.1.0}
     end
   end
 end
